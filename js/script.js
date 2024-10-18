81901918dd5d27ccaf0143dc92e8d03e7e736fa8
박정인 <jeongin@hivelab.co.kr>
2024-10-18 19:22:30 +0900

    feat: detail,partial,js추가

--- js/script.js
+++ js/script.js
@@ -1,10 +1,52 @@
+
+
+//get query string parameter
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+Handlebars.registerHelper("formatName", function(property1, property2){
+return new Handlebars.SafeString(
+  "Hello my name is <strong>" + property1 + "</strong> and I live at <strong>" + property2 + "</strong>."
+);
+});
+Handlebars.registerHelper("formatPhoneNumber", function(property) {
+if (property) {
+  var phone = property.toString();
+  return "(" + phone.substr(0, 3) + ")" + phone.substr(3, 3) + "-" + phone.substr(6,4);
+}
+});
+Handlebars.registerHelper("makeBold", function(options) {
+return new Handlebars.SafeString("<strong>" + options.fn(this) + "</strong>");
+});
+Handlebars.registerHelper("toLower", function(options) {
+return options.fn(this).toLowerCase();
+});
+
 $(document).ready(function(){
-  var characterTemplate = $("#character-template").html();
-  var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
+var characterTemplate = $("#character-template").html();
 
-  $.ajax("./data/cast.json").done(function(cast){
-    $(".character-list-container").html(compiledCharacterTemplate(cast))
-  });
+var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
+var $characterList = $(".character-list-container");
 
+var characterId = getParameterByName("id");
+$.ajax("./character-details-partial.html").done(function(charDetailsPartial) {
+  $("body").append(charDetailsPartial);
+  Handlebars.registerPartial("characterDetailsPartial", $("#character-details-partial").html());
+});
+$.ajax("./data/cast.json").done(function(cast) {
+  if ($("body").hasClass("page-cast-details")) {
+    $characterList.html(compiledCharacterTemplate(cast.characters[characterId]));
+  } else {
+    $characterList.html(compiledCharacterTemplate(cast));
+  }	
+});
 });
 
