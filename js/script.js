3170b05846c5a5bb64e676ed873686969f7de503
박정인 <jeongin@hivelab.co.kr>
2024-10-21 16:33:56 +0900

    feat: handlebars 완료

--- js/script.js
+++ js/script.js
@@ -12,41 +12,36 @@ function getParameterByName(name, url) {
 }
 
 
-Handlebars.registerHelper("formatName", function(property1, property2){
-return new Handlebars.SafeString(
-  "Hello my name is <strong>" + property1 + "</strong> and I live at <strong>" + property2 + "</strong>."
-);
+Handlebars.registerHelper("formatName", function(property1){
+  return new Handlebars.SafeString(
+    "<strong>" + property1 + "</strong>"
+  );
 });
-Handlebars.registerHelper("formatPhoneNumber", function(property) {
-if (property) {
-  var phone = property.toString();
-  return "(" + phone.substr(0, 3) + ")" + phone.substr(3, 3) + "-" + phone.substr(6,4);
-}
-});
+
 Handlebars.registerHelper("makeBold", function(options) {
-return new Handlebars.SafeString("<strong>" + options.fn(this) + "</strong>");
-});
-Handlebars.registerHelper("toLower", function(options) {
-return options.fn(this).toLowerCase();
+  return new Handlebars.SafeString("<strong>" + options.fn(this) + "</strong>");
 });
 
 $(document).ready(function(){
-var characterTemplate = $("#character-template").html();
+  var characterTemplate = $("#character-template").html();
+  var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
+  var characterList = $(".character-list-container");
+  var characterId = getParameterByName("id");
 
-var compiledCharacterTemplate = Handlebars.compile(characterTemplate);
-var $characterList = $(".character-list-container");
+  $.ajax("./character-details-partial.html").done(function(charDetailsPartial) {
+    console.log(charDetailsPartial);
+    $("body").append(charDetailsPartial);
+    Handlebars.registerPartial("characterDetailsPartial", $("#character-details-partial").html());
+  });
+  
+  $.ajax("./data/cast.json").done(function(cast) {
+    console.log(cast);
+    if ($("body").hasClass("page-cast-details")) {
+      characterList.html(compiledCharacterTemplate(cast.characters[characterId]));
+    } else {
+      characterList.html(compiledCharacterTemplate(cast));
+    }	
+  });
 
-var characterId = getParameterByName("id");
-$.ajax("./character-details-partial.html").done(function(charDetailsPartial) {
-  $("body").append(charDetailsPartial);
-  Handlebars.registerPartial("characterDetailsPartial", $("#character-details-partial").html());
-});
-$.ajax("./data/cast.json").done(function(cast) {
-  if ($("body").hasClass("page-cast-details")) {
-    $characterList.html(compiledCharacterTemplate(cast.characters[characterId]));
-  } else {
-    $characterList.html(compiledCharacterTemplate(cast));
-  }	
-});
 });
 
